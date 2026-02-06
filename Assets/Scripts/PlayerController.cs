b0257547056629e3cb0efbb3021c3e7c15598c28
DanielRTato <drodrigueztato@danielcastelao.org>
2026-02-06 11:22:12 +0100

    Añadir salta y zona de muerte

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,33 +3,61 @@ using UnityEngine.InputSystem;
 using TMPro;
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody rb; 
-    
-    [SerializeField] float speed = 0; // Velocidad del jugador 
+    private Rigidbody rb;
+
+    [SerializeField] float speed = 0; // Velocidad del jugador
+    [SerializeField] float jumpForce = 7f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0.2f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] float fallLimit = -10f;
+
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
-    
+
     private int count;
     private float movementX;
     private float movementY;
+    private bool isGrounded;
+
+    InputAction jumpAction;
+
     void Start()
     {
-        rb = GetComponent <Rigidbody>(); 
-        count = 0; 
+        rb = GetComponent <Rigidbody>();
+        count = 0;
         winTextObject.SetActive(false);
+        jumpAction = InputSystem.actions.FindAction("Jump");
     }
     void OnMove (InputValue movementValue)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>(); 
-        movementX = movementVector.x; 
-        movementY = movementVector.y; 
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (jumpAction.WasPressedThisFrame() && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if (transform.position.y < fallLimit)
+        {
+            Destroy(gameObject);
+            winTextObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
     }
     
     void SetCountText() 
     {
         countText.text =  "Count: " + count.ToString();
-        if (count >= 4)
+        if (count >= 6)
         {
             winTextObject.SetActive(true);
             Destroy(GameObject.FindGameObjectWithTag("Enemy"));
